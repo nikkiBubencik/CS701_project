2f5e82ca332d956cc511af04a41efd56a389882b
nikkibubencik <bubnikki4@gmail.com>
2025-06-25 22:01:53 -0400

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,7 +2,7 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: 'https://github.com/nikkiBubencik/CS701_project',
+  baseHref: 'https://github.com/nikkiBubencik/CS701_project/',
   locale: undefined,
   routes: [
   {
@@ -40,12 +40,12 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 733, hash: '96c5e6300298f7b9a194976f4b71309124f4dbfe5a63b41ec6845faea2638168', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1062, hash: '89b5558cfbf18dcc53d406da086b9fef13b5f0f691bac556d9dffea4e7cd03ed', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'search/index.html': {size: 3937, hash: 'b31e131ea40c98780c396f7c1fe2258bced5584cc727b4686e1d4c5f50ac7815', text: () => import('./assets-chunks/search_index_html.mjs').then(m => m.default)},
-    'add/index.html': {size: 3473, hash: '2f56399af0d5a244e6c9b0636ade27c95546ad2b21ea1c5ca457184cd135e1bc', text: () => import('./assets-chunks/add_index_html.mjs').then(m => m.default)},
-    'readingLists/index.html': {size: 3467, hash: '132ed4c10dc66497a89b7973e1b265d7bb0ecac8e808acd5dd335adfe928436c', text: () => import('./assets-chunks/readingLists_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 3596, hash: '0bcfccf1434b90548c8e3a0be38f52b8713ced70f98e54f2467513b3eda1effb', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 734, hash: 'a5514b531dc0d83afc0b9f97ed69468d471e8f10d293d19beb39f94f72000273', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1063, hash: '6ea452d2fe27015179bf0dcd2da6cb6a1a60bb40719ca573c5941b545f6707ff', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'search/index.html': {size: 3938, hash: '9d15eb4d26d6ccaad4e96a5fd69fd66ae697c1d6b92aa6a10cd305bf8f00ba0f', text: () => import('./assets-chunks/search_index_html.mjs').then(m => m.default)},
+    'index.html': {size: 3597, hash: '912420e169e9e57d6839198bf990ae498cf1569a69cbbf11c45ccd9241f10e8b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'readingLists/index.html': {size: 3468, hash: 'de4f15de74aaf23ce2060944785e8816f05d8e92b8a90e3af5a676eaa2aa72d5', text: () => import('./assets-chunks/readingLists_index_html.mjs').then(m => m.default)},
+    'add/index.html': {size: 3474, hash: '10944b622e0a2c8a6057a3ec6e9247e28fffd87abb5b19e060258ebe3ca9f76a', text: () => import('./assets-chunks/add_index_html.mjs').then(m => m.default)},
     'styles-DC7VQKHW.css': {size: 75, hash: 'Nqj9yR3pnd8', text: () => import('./assets-chunks/styles-DC7VQKHW_css.mjs').then(m => m.default)}
   },
 };
--- server/assets-chunks/index_server_html.mjs
+++ server/assets-chunks/index_server_html.mjs
@@ -3,7 +3,7 @@ export default `<!doctype html>
 <head>
   <meta charset="utf-8">
   <title>Cs701ProjectBubencik</title>
-  <base href="https://github.com/nikkiBubencik/CS701_project">
+  <base href="https://github.com/nikkiBubencik/CS701_project/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 <link rel="stylesheet" href="styles-DC7VQKHW.css"></head>
